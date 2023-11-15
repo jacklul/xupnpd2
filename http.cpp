a8f13c088089ee5959685e26d9bbf1bbf3960c66
Jack'lul <jacklulcat@gmail.com>
2023-11-15 18:35:05 +0100

    Remove this piece of useless includes added in 53c0e7f

--- http.cpp
+++ http.cpp
@@ -30,21 +30,6 @@
 #include <winsock2.h>
 #endif /* _WIN32 */
 
-#ifndef __has_include
-  static_assert(false, "__has_include not supported");
-#else
-#  if __cplusplus >= 201703L && __has_include(<filesystem>)
-#    include <filesystem>
-     namespace fs = std::filesystem;
-#  elif __has_include(<experimental/filesystem>)
-#    include <experimental/filesystem>
-     namespace fs = std::experimental::filesystem;
-#  elif __has_include(<boost/filesystem.hpp>)
-#    include <boost/filesystem.hpp>
-     namespace fs = boost::filesystem;
-#  endif
-#endif
-
 namespace http
 {
     volatile bool quit=false;
