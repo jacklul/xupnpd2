992cf8662774daef9ed9b53af0d89a9765f56b48
Jack'lul <jacklulcat@gmail.com>
2026-06-12 17:01:05 +0200

    Fix empty user-agent string being inserted to extra column

--- scan.cpp
+++ scan.cpp
@@ -393,7 +393,9 @@ int utils::scan_playlist(const std::string& path,int parentid,int& objid,std::st
                 else if (track_raw == "false" || track_raw == "0")
                     track_extra.set("raw", "false");
 
-                track_extra.set("user-agent", track_ext["user-agent"].c_str());
+                if (!track_ext["user-agent"].empty()) {
+                    track_extra.set("user-agent", track_ext["user-agent"].c_str());
+                }
 
                 if(t && t->upnp_proto)
                 {
